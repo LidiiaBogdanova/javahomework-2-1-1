9acceea9b15fd8e8e9001cdbeac9c431b1e8dd27
LidiiaBogdanova <novikova71095@gmail.com>
2021-03-13 00:19:31 +0300

    files added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 25738;
+        int miles = ticket_price / 20;
+
+        System.out.println(miles);
+
+    }
+}
